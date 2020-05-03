ee0ff4c66b7d3ebd77abd439e3343e40b88d68a7
John <zjohnzheng@gmail.com>
2020-05-02 21:23:59 -0700

    Initial Commit

---
+++ Projector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Projector
+//
+//  Created by Zheng on 5/2/20.
+//  Copyright © 2020 Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
